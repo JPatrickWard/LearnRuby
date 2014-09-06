58280b13551fed09fa64329bed78b188905a4048
patrick.ward <patrick.ward@octanner.com>
2014-09-05 20:09:06 -0600

    Week 3 - Web BlackJack - Checkin but not yet working

--- TeaLeaf_Academy/Homework/Week_3_Sinatra_Blackjack/main.rb
+++ TeaLeaf_Academy/Homework/Week_3_Sinatra_Blackjack/main.rb
@@ -3,21 +3,68 @@ require 'sinatra'
 
 set :sessions, true
 
+BLACKJACK_AMOUNT = 21
+DEALER_MIN = 17
+DOLLAR_START_AMOUNT = 500
 
-get '/' do  # This handles when I go to localhost:9393
-  # if user exists, then progress to rest of the game.
-  # Else redirect to new user forum.
+helpers do
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
+
+    total = 0
+    arr.each do |a|
+      if a == "A"
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+    #correct for aces
+    arr.select{|element| element == "A"}.count.times do
+      break if total <= BLACKJACK_AMOUNT
+      total -=10
+    end
+    total
+  end
+
+  def card_image(card)
+    suit = case card[0]
+             when 'H' then 'hearts'
+             when 'D' then 'diamonds'
+             when 'C' then 'clubs'
+             when 'S' then 'spades'
+           end
+
+    value = card[1]
+    if %w(J Q K A).include? value
+      value = case card[1]
+                when 'J' then 'jack'
+                when 'Q' then 'queen'
+                when 'K' then 'king'
+                when 'A' then 'ace'
+              end
+    end
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
+
+end
+
+before do
+  @show_hit_or_stay_buttons = true
+end
+# This handles when I go to localhost:9393 # if user exists, then progress to rest of the game. # Else redirect to new user forum.
+get '/' do
   if session[:player_name]
     # progress to the game
     redirect '/game'
   else
     redirect '/new_player'
   end
-
 end
 
-get '/new_player' do
+get '/new_player' do  # get is a 'redirect'
   erb :new_player
+  # erb specifies how to render the form. Or, rather the new tab.
 end
 
 post '/new_player' do
@@ -27,13 +74,10 @@ post '/new_player' do
 end
 
 get '/game' do
-  # setup initial game values
-  # render the template
-  # Initialize Game
-
   suits = ['C', 'H', 'S', 'D']
   values = ['K', 'A', 'J', 'Q', '10', '6', '2', '7', '3', '9', '4', '8', '5']
   session[:deck] = suits.product(values).shuffle!
+
   # Deal Cards
   session[:dealer_cards] = []
   session[:player_cards] = []
@@ -42,8 +86,21 @@ get '/game' do
   session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
 
-  erb:game
+  erb :game
+end
 
-  # will Need Dealer Cards
-    # Will need Player Cards
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  if calculate_total(session[:player_cards]) > BLACKJACK_AMOUNT
+    @error = "You busted."
+    @show_hit_or_stay_buttons = false
+  end
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "You will stay."
+  @show_hit_or_stay_buttons = false
+
+  erb :game
 end
